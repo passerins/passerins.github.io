ae7a1524326484c174aff3af26352980209a2795
Chandler <me@cseverson.com>
2020-06-01 21:36:18 -0700

    Make Contact me CTA modular

--- js/load-common-elements.js
+++ js/load-common-elements.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
     $('#footer').load("../template/footer.html");
     $('#header').load("../template/header.html");
     $('#tagline').load("../template/tagline.html");
+    $('#contact-cta').load("../template/contact-cta.html");
 });
