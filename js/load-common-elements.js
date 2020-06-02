57291645720e6e3ff6a9f081b7f854f006195ee8
Chandler <me@cseverson.com>
2020-06-01 20:19:22 -0700

    Modularize html files and update contact form

---
+++ js/load-common-elements.js
@@ -0,0 +1,6 @@
+'use strict';
+$(document).ready(function(){
+    $('#footer').load("../template/footer.html");
+    $('#header').load("../template/header.html");
+    $('#tagline').load("../template/tagline.html");
+});
