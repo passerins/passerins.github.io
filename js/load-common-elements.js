89d34acb2e9d94333954342052a2106f29f7fc07
Chandler <me@cseverson.com>
2020-09-03 21:24:02 -0700

    Add google analytics

--- js/load-common-elements.js
+++ js/load-common-elements.js
@@ -1,5 +1,17 @@
 'use strict';
+
+const analyticsObject = `<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-153116058-2"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-153116058-2');
+</script>`;
+
 $(document).ready(function(){
+    $('head').append(analyticsObject);
     $('#footer').load("/template/footer.html");
     $('#header').load("/template/header.html");
     $('#tagline').load("/template/tagline.html");
